971c7b13458a6f508ade9639f5fceddd7fc15b9a
jpfernz <jfwintersdev@gmail.com>
2024-06-25 12:22:13 +0100

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonsComponent } from './components/buttons/buttons.component';
+import { Observable } from 'rxjs';
+import { ThemeService } from './services/theme.service';
+import { AsyncPipe } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonsComponent],
+  imports: [RouterOutlet, MatSlideToggleModule, ButtonsComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'features-app';
+  private themeService = inject(ThemeService);
+
+  isDarkTheme!: Observable<boolean>;
+
+  ngOnInit() {
+    this.isDarkTheme = this.themeService.isDarkTheme;
+  }
+
+  toggleDarkTheme(checked: boolean) {
+    this.themeService.setDarkTheme(checked);
+  }
 }
